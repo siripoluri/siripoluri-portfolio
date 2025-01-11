08b5ed8706a16dc621c5983a3dadf64d4247c37d
Nagasiri Poluri <nagasiripoluri@Siris-MacBook-Air.local>
2025-01-11 15:29:57 -0700

    Updated LinkedIn link and fixed  navigation issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,31 @@ import { CONTACT } from "../constants";
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
-        <div className="text-center trackin-tighter">
-            <p className="my-4"><a href="#" className="border-b mb-2">{CONTACT.email}</a></p>
-            <p className="my-4"><a href="#" className="border-b">{CONTACT.LinkedIn}</a></p>
-        </div> 
+      <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+      <div className="text-center tracking-tighter">
+        <p className="my-4">
+          <a 
+            href={`mailto:${CONTACT.email}`} 
+            className="border-b mb-2" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            aria-label="Email"
+          >
+            {CONTACT.email}
+          </a>
+        </p>
+        <p className="my-4">
+          <a 
+            href={CONTACT.LinkedIn}
+            className="border-b" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            aria-label="LinkedIn"
+          >
+            Visit my LinkedIn profile
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -61,5 +61,5 @@ export const CONTACT = {
   address: "767 Fifth Avenue, New York, NY 10153 ",
   phoneNo: "+12 4555 666 00 ",
   email: "siripoluri09@gmail.com",
-  LinkedIn: "www.linkedin.com/in/siri-poluri"
+  LinkedIn: "https://www.linkedin.com/in/siri-poluri"
 };
